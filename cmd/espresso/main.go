281300f405ff15f51b6874a1ba504910abde5c2c
alconaut <alconautgh@yandex.com>
2015-09-19 22:48:32 +0600

    Added a command for parsing espresso

---
+++ cmd/espresso/main.go
@@ -0,0 +1,30 @@
+// Command espresso parses Espresso as defined by [1].
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/anonx/espresso/parser"
+)
+
+func main() {
+	in := os.Stdin
+	nm := "stdin"
+	if len(os.Args) > 1 {
+		f, err := os.Open(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		in = f
+		nm = os.Args[1]
+	}
+
+	got, err := parser.ParseReader(nm, in)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(got)
+}
