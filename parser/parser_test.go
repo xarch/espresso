1f6e109ce92706fef555cea2de22e403fab3d226
alconaut <alconautgh@yandex.com>
2015-09-20 12:00:31 +0600

    Better test coverage

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,12 +7,50 @@ import (
 	ts "github.com/anonx/espresso/parser/testdata"
 )
 
-func TestParseFile(t *testing.T) {
-	res, err := ParseFile("./testdata/basic.espresso")
-	if err != nil {
-		t.Error(err)
+var correctFiles = map[string]interface{}{
+	"./testdata/basic.espresso": ts.Basic,
+}
+
+var incorrectFiles = []string{
+	"./testdata/unclosedBraces.espresso",
+}
+
+func TestParseFile_NonExistent(t *testing.T) {
+	res, err := ParseFile("file_that_does_not_exist")
+	if res != nil || err == nil {
+		t.Error("File does not exist, error expected.")
 	}
-	if !reflect.DeepEqual(ts.Basic, res) {
-		t.Errorf("Incorrect result of ParseFile. Expected:\n%v\nGot:\n%v", ts.Basic, res)
+}
+
+func TestParseFile_Incorrect(t *testing.T) {
+	for _, path := range incorrectFiles {
+		res, err := ParseFile(path, Debug(true))
+		if res != nil || err == nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestParseFile(t *testing.T) {
+	for path, exp := range correctFiles {
+		res, err := ParseFile(path)
+		if err != nil {
+			t.Error(err)
+		}
+		if !reflect.DeepEqual(exp, res) {
+			t.Errorf("Incorrect result of ParseFile. Expected:\n%v\nGot:\n%v", exp, res)
+		}
+	}
+}
+
+func TestParseFile_Debug(t *testing.T) {
+	for path, exp := range correctFiles {
+		res, err := ParseFile(path, Debug(true), Memoize(true), Recover(true))
+		if err != nil {
+			t.Error(err)
+		}
+		if !reflect.DeepEqual(exp, res) {
+			t.Errorf("Incorrect result of ParseFile. Expected:\n%v\nGot:\n%v", exp, res)
+		}
 	}
 }
